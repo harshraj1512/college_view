3a12ccb025072b69ef5b9abe133d9f72d2c6da56
harshraj1512 <harshraj1512@gmail.com>
2023-09-21 15:54:27 +0530

    First working version

--- app/src/main/java/com/example/collegelist/UniversityAdapter.kt
+++ app/src/main/java/com/example/collegelist/UniversityAdapter.kt
@@ -21,13 +21,6 @@ class UniversityAdapter(private var universities: MutableList<University>) :
         val university = universities[position]
         holder.universityNameTextView.text = university.name
 
-        if (isExpandedList[position]) {
-            holder.websiteTextView.visibility = View.VISIBLE
-            holder.countryTextView.visibility = View.VISIBLE
-        } else {
-            holder.websiteTextView.visibility = View.GONE
-            holder.countryTextView.visibility = View.GONE
-        }
         // Bind other data as needed
     }
 
@@ -51,17 +44,8 @@ class UniversityAdapter(private var universities: MutableList<University>) :
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val universityNameTextView: TextView = itemView.findViewById(R.id.universityNameTextView)
         // Add references to other UI elements here
-        val websiteTextView: TextView = itemView.findViewById(R.id.websiteTextView)
-        val countryTextView: TextView = itemView.findViewById(R.id.countryTextView)
-        init {
-            universityNameTextView.setOnClickListener {
-                val position = bindingAdapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    isExpandedList[position] = !isExpandedList[position]
-                    notifyItemChanged(position)
-                }// Notify the adapter that data has changed
-            }
-        }
+
+
     }
 
 }
